b295c7a15efe30c76febcac15c605ac70a5601c4
DiogoLogic <diogologic@gmail.com>
2024-08-03 15:08:48 -0300

    utilização do @CrossOrigin

--- api/src/main/java/br/com/projaeto/angular/springboot/api/controller/Controller.java
+++ api/src/main/java/br/com/projaeto/angular/springboot/api/controller/Controller.java
@@ -1,8 +1,12 @@
 package br.com.projaeto.angular.springboot.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,7 @@ import br.com.projaeto.angular.springboot.api.model.Cliente;
 import br.com.projaeto.angular.springboot.api.repository.Repositorio;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controller {
 
     @Autowired
@@ -23,5 +28,15 @@ public class Controller {
     @GetMapping("/")
     public Iterable<Cliente> selecionar(){
         return acao.findAll();
+    }
+
+    @PutMapping("/")
+    public Cliente editar(@RequestBody Cliente c){
+    return acao.save(c);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable long codigo){
+        acao.deleteById(codigo);
     }
 }
